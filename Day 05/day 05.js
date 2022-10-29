9df145102fe5eea84546f0efcf910b815a831a75
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-10-29 14:07:07 -0300

    dia 5 concluido

---
+++ Day 05/day 05.js
@@ -0,0 +1,30 @@
+let inicia = prompt("Deseja inserir algum item na sua lista?")
+
+let frutas = []
+let laticinios = []
+let congelados = []
+let doces = []
+
+
+
+if(inicia == "sim") {
+    while(inicia.toLocaleLowerCase() == "sim") {
+        let item = prompt("Oque você deseja inserir?")
+        let categoria = prompt("Qual a categoria do item? 1: frutas, 2: laticínios, 3: congelados, 4: doces")
+        switch (categoria) {
+            case "1" : frutas.push(item); break;
+            case "2" : laticinios.push(item); break;
+            case "3" : congelados.push(item); break;
+            case "4" : doces.push(item); break
+        }
+        inicia = prompt("Deseja inserir algum item na sua lista?")
+    }
+
+    alert(`Lista de compras:
+            Frutas: ${frutas}
+            Laticínios: ${laticinios}
+            Congelados: ${congelados}
+            Doces: ${doces}`)
+}
+
+alert("Obrigado por usar o programa ^^ .")
